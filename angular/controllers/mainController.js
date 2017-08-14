5da6cb9d3e6ee6a183fd894f366a199313559f72
abhijeetjuneja <abhijeetjuneja2014@gmail.com>
2017-08-15 00:45:25 +0530

    Main height

--- angular/controllers/mainController.js
+++ angular/controllers/mainController.js
@@ -1,9 +1,11 @@
 app.controller('mainController',function(){
 	//Adjust jumbotron padding
 	$( document ).ready(function() {
-		var h1=document.getElementById('nav1').scrollHeight;
-		var h2=document.getElementById('jumbo').scrollHeight;
-		var h3=document.getElementById('myFooter').scrollHeight;
+		var h1=document.getElementById('nav1').offsetHeight;
+		document.getElementById('jumbo').style.paddingTop=0;
+		document.getElementById('jumbo').style.paddingBottom=0;
+		var h2=document.getElementById('jumbo').offsetHeight;
+		var h3=document.getElementById('myFooter').offsetHeight;
 		var h4=window.innerHeight-(h1+h2+h3);
 		h4=h4/2;
 		console.log(h1,h2,h3,h4,window.innerHeight);
@@ -15,13 +17,16 @@ app.controller('mainController',function(){
 
 	//Adjust jumbotron padding on orientation change
 	$(window).on("orientationchange",function(){
-		var h1=document.getElementById('nav1').scrollHeight;
-		var h2=document.getElementById('jumbo').scrollHeight;
-		var h3=document.getElementById('myFooter').scrollHeight;
+		var h1=document.getElementById('nav1').offsetHeight;
+		document.getElementById('jumbo').style.paddingTop=0;
+		document.getElementById('jumbo').style.paddingBottom=0;
+		var h2=document.getElementById('jumbo').offsetHeight;
+		var h3=document.getElementById('myFooter').offsetHeight;
 		var h4=window.innerHeight-(h1+h2+h3);
 		h4=h4/2;
 		var s="";
 		s=h4+"px";
+		console.log(h1,h2,h3,h4,window.innerHeight);
 		document.getElementById('jumbo').style.paddingTop=s;
 		document.getElementById('jumbo').style.paddingBottom=s;
 	});
