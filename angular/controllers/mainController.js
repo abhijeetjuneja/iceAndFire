15f1dcf78f396f9983a086a56b14708678cd3647
abhijeetjuneja <abhijeetjuneja2014@gmail.com>
2017-08-17 22:29:10 +0530

    orientation

--- angular/controllers/mainController.js
+++ angular/controllers/mainController.js
@@ -12,24 +12,41 @@ app.controller('mainController',function(){
 		s=h4+"px";
 		document.getElementById('jumbo').style.paddingTop=s;
 		document.getElementById('jumbo').style.paddingBottom=s;
-
-
 	});
 
 	//Adjust jumbotron padding on orientation change
-	$(window).on("orientationchange",function(){
-		var h1=document.getElementById('nav1').offsetHeight;
-		document.getElementById('jumbo').style.paddingTop=0;
-		document.getElementById('jumbo').style.paddingBottom=0;
-		var h2=document.getElementById('jumbo').offsetHeight;
-		var h3=document.getElementById('myFooter').offsetHeight;
-		var h4=window.innerHeight-(h1+h2+h3);
-		h4=h4/2;
-		var s="";
-		s=h4+"px";
-		console.log(h1,h2,h3,h4,window.innerHeight);
-		document.getElementById('jumbo').style.paddingTop=s;
-		document.getElementById('jumbo').style.paddingBottom=s;
-	});
+	
+   $(window).on('orientationchange', function(event) {
+        console.log(window.orientation);
+        if(window.orientation==0||window.orientation==180)
+        {
+        	var h1=document.getElementById('nav1').offsetHeight;
+			document.getElementById('jumbo').style.paddingTop=0;
+			document.getElementById('jumbo').style.paddingBottom=0;
+			var h2=document.getElementById('jumbo').offsetHeight;
+			var h3=document.getElementById('myFooter').offsetHeight;
+			var h4=window.innerHeight-(h1+h2+h3);
+			h4=h4/2;
+			s=h4+"px";
+			document.getElementById('jumbo').style.paddingTop=s;
+			document.getElementById('jumbo').style.paddingBottom=s;
+        }
+        if(window.orientation==90||window.orientation==-90)
+        {
+        	var h1=document.getElementById('nav1').offsetHeight;
+			document.getElementById('jumbo').style.paddingTop=0;
+			document.getElementById('jumbo').style.paddingBottom=0;
+			var h2=document.getElementById('jumbo').offsetHeight;
+			var h3=document.getElementById('myFooter').offsetHeight;
+			var h4=window.innerHeight-(h1+h2+h3);
+			h4=h4/2;
+			s=h4+"px";
+			document.getElementById('jumbo').style.paddingTop=s;
+			document.getElementById('jumbo').style.paddingBottom=s;
+        }
+        
+    });
+
+ 
 
 });
